4cc5c6e335b6941ef2f3cab1dea36b1ac70d33ea
Evan Thibodeau <evan.thibodeau@liferay.com>
2019-11-27 09:02:58 +0400

    MVP

--- src/AppComponent.js
+++ src/AppComponent.js
@@ -1,28 +1,196 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import ClayAlert from '@clayui/alert';
+import ClayForm, {ClayInput} from '@clayui/form';
+import ClayLoadingIndicator from '@clayui/loading-indicator';
+import ClayTable from '@clayui/table';
+
+const loadingStyles = {
+	backgroundColor: 'rgba(0,0,0,0.2)',
+	borderRadius: '5px',
+	height: '100%',
+	width: '100%',
+	zIndex: 1
+};
+
 export default class extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			city: 'London',
+			country: 'UK',
+			timings: {
+				data: {},
+				loading: false
+			}
+		};
+
+		this._fetchTimings = this._fetchTimings.bind(this);
+		this._handleCityChange = this._handleCityChange.bind(this);
+		this._handleCountryChange = this._handleCountryChange.bind(this);
+		this._handleSubmit = this._handleSubmit.bind(this);
+	}
+
+	_fetchTimings() {
+		this.setState({
+			timings: {
+				data: this.state.timings.data,
+				loading: true
+			}
+		});
+
+		const {city, country} = this.state;
+
+		const response = fetch(`http://api.aladhan.com/v1/timingsByCity?city=${city}&country=${country}&method=8`)
+			.then(res => res.json())
+			.then(
+				(result) => {
+					console.log(result);
+
+					if (result.code == 200) {
+						this.setState({
+							timings: {
+								data: result.data.timings,
+								loading: false
+							}
+						});
+					}
+					else {
+						this.setState({
+							timings: {
+								data: this.state.timings.data,
+								error: 'There was an error processing your request.',
+								loading: false
+							}
+						});
+
+					}
+				}
+			);
+	}
+
+	_handleCityChange(event) {
+		console.log(event);
+
+		this.setState({
+			city: event.target.value
+		})
+	}
+
+	_handleCountryChange(event) {
+		console.log(event);
+
+		this.setState({
+			country: event.target.value
+		})
+	}
+
+	_handleSubmit(event) {
+		event.preventDefault();
+
+		this._fetchTimings();
+	}
+
 	render() {
+		const {city, country} = this.state;
+
+		const timings = this.state.timings.data;
+		const {error, loading} = this.state.timings;
+
 		return (
-            <div>
-				<div>
-        	        <span className="tag">{Liferay.Language.get('portlet-namespace')}:</span> 
-					<span className="value">{this.props.portletNamespace}</span>
-				</div>
-				<div>
-    	            <span className="tag">{Liferay.Language.get('context-path')}:</span>
-					<span className="value">{this.props.contextPath}</span>
-				</div>
-				<div>
-	                <span className="tag">{Liferay.Language.get('portlet-element-id')}:</span>
-					<span className="value">{this.props.portletElementId}</span>
-				</div>
+			<div>
+				<ClayForm onSubmit={this._handleSubmit}>
+					<ClayForm.Group className="form-group-autofit">
+						<div className="form-group-item">
+							<label htmlFor="country">Country</label>
+
+							<ClayInput
+								id="country"
+								onChange={this._handleCountryChange}
+								placeholder="United Arab Emirates"
+								type="text"
+								value={country}
+							/>
+						</div>
+
+						<div className="form-group-item">
+							<label htmlFor="city">City</label>
+
+							<ClayInput
+								id="city"
+								onChange={this._handleCityChange}
+								placeholder="Abu Dhabi"
+								type="text"
+								value={city}
+							/>
+						</div>
+					</ClayForm.Group>
+					<ClayForm.Group className="form-group-autofit">
+						<div className="form-group-item">
+							<ClayInput
+								className="btn btn-primary"
+								type="submit"
+								value="Get Prayer Times"
+							/>
+						</div>
+					</ClayForm.Group>
+				</ClayForm>
 				
-				<div>
-					<span className="tag">{Liferay.Language.get('configuration')}:</span>
-					<span className="value pre">{JSON.stringify(this.props.configuration, null, 2)}</span>
+				<h1>Timings</h1>
+
+				{error &&
+					<ClayAlert displayType="warning" title="Error">
+						{error}
+					</ClayAlert>
+				}
+
+				<div className="position-relative">
+
+					{loading &&
+						<div className="align-items-center d-flex justify-content-center position-absolute" style={loadingStyles}>
+							<ClayLoadingIndicator />
+						</div>
+					}
+
+					<ClayTable>
+						<ClayTable.Body>
+							<ClayTable.Row>
+								<ClayTable.Cell>{"Imsak"}</ClayTable.Cell>
+								<ClayTable.Cell>{timings.Imsak && timings.Imsak}</ClayTable.Cell>
+							</ClayTable.Row>
+							<ClayTable.Row>
+								<ClayTable.Cell>{"Fajr"}</ClayTable.Cell>
+								<ClayTable.Cell>{timings.Fajr && timings.Fajr}</ClayTable.Cell>
+							</ClayTable.Row>
+							<ClayTable.Row>
+								<ClayTable.Cell>{"Sunrise"}</ClayTable.Cell>
+								<ClayTable.Cell>{timings.Sunrise && timings.Sunrise}</ClayTable.Cell>
+							</ClayTable.Row>
+							<ClayTable.Row>
+								<ClayTable.Cell>{"Dhuhr"}</ClayTable.Cell>
+								<ClayTable.Cell>{timings.Dhuhr && timings.Dhuhr}</ClayTable.Cell>
+							</ClayTable.Row>
+							<ClayTable.Row>
+								<ClayTable.Cell>{"Asr"}</ClayTable.Cell>
+								<ClayTable.Cell>{timings.Asr && timings.Asr}</ClayTable.Cell>
+							</ClayTable.Row>
+							<ClayTable.Row>
+								<ClayTable.Cell>{"Maghrib"}</ClayTable.Cell>
+								<ClayTable.Cell>{timings.Maghrib && timings.Maghrib}</ClayTable.Cell>
+							</ClayTable.Row>
+							<ClayTable.Row>
+								<ClayTable.Cell>{"Isha"}</ClayTable.Cell>
+								<ClayTable.Cell>{timings.Isha && timings.Isha}</ClayTable.Cell>
+							</ClayTable.Row>
+							<ClayTable.Row>
+								<ClayTable.Cell>{"Midnight"}</ClayTable.Cell>
+								<ClayTable.Cell>{timings.Midnight && timings.Midnight}</ClayTable.Cell>
+							</ClayTable.Row>
+						</ClayTable.Body>
+					</ClayTable>
 				</div>
-				
 			</div>
 		);
 	}	
